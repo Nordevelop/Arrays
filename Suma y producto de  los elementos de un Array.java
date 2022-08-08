38d5df809477d8b28bc70846638c5f0aecfe13d9
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-08-07 22:16:25 -0300

    Update Suma y producto de  los elementos de un Array.java

--- Suma y producto de  los elementos de un Array.java
+++ Suma y producto de  los elementos de un Array.java
@@ -1,1 +1,21 @@
 package misClases;
+import java.util.Arrays;
+public class Main{
+  public static void main(string[] args) {
+    /*Author Nordevelop
+		 * 
+		 * <>[]
+		 */
+    int []arr = new int []{10,5,3,1};
+    int sum = 0;
+    int prod = 1;
+    for (int i=0;i < arr.length;i++){
+      sum = sum + arr[i];
+      prod = prod * arr[i];
+    }
+    for(int n:arr) //for each
+      System.out.println(n);
+    System.out.println
+    
+      }
+}

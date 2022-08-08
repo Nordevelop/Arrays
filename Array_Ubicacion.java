2dd7a9899efef2abd6372ae7609436931f0b4d9d
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-08-07 23:54:24 -0300

    Update Array_Ubicacion.java

--- Array_Ubicacion.java
+++ Array_Ubicacion.java
@@ -9,7 +9,16 @@ public class Main {
     */<>[]
     int []arr = new int []{0,1,2,3,4,5,6,7,8,9,10};
      System.out.println(" los elementos del Array Completo. ");
-     System.out.println
-     }
+     for (int i=0; i<arr.length; i++ )
+        System.out.print(arr[i] + " ");
+     System.out.println(" ");    
+     System.out.println("Los elementos del Array en los subindices Par: ");
+     for(int j =0; j<arr.length; j=j+2)
+        System.out.print(arr[i] + " ")
+     System.out.println("Los elementos del Array en los subindices Impar: ");  
+     for(int k=1; k<arr.length; k=k+2)
+        System.out.print(arr[k] + " ")
+        
+      }
 }
       
